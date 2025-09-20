dec80a09e3f306d226276d531099a194eb62b66a
muhamad ihsan <muhamadihsan06381@gmail.com>
2025-09-20 15:22:32 +0700

    baru lagi

--- resources/views/masterdata/madrasah/index.blade.php
+++ resources/views/masterdata/madrasah/index.blade.php
@@ -83,7 +83,7 @@
                                                 width="50" class="img-thumbnail" style="object-fit: contain;">
                                         @elseif(file_exists(public_path('public/' . $logoPath)))
                                             {{-- Jika file tersimpan langsung di public/storage --}}
-                                            <img src="{{ asset('storage/app/public/madrasah/' . $logoPath) }}"
+                                            <img src="{{ asset('storage/app/public/' . $logoPath) }}"
                                                 alt="Logo {{ $madrasah->name }}"
                                                 width="50" class="img-thumbnail" style="object-fit: contain;">
                                         @else
