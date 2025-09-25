f51b60bc07eed63ea49f9f327fbb2894754767b7
muhamad ihsan <muhamadihsan06381@gmail.com>
2025-09-25 20:53:47 +0700

    new

---
+++ storage/framework/views/07d43be80f74bf99a0dbf4a42a0c0a38.php
@@ -0,0 +1,175 @@
+<?php $__env->startSection('title'); ?>
+<?php echo app('translator')->get('translation.Login'); ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+
+<?php echo app('Illuminate\Foundation\Vite')(['resources/scss/bootstrap.scss', 'resources/scss/icons.scss', 'resources/scss/app.scss', 'resources/js/app.js']); ?>
+
+
+<link rel="stylesheet" href="<?php echo e(asset('build/libs/owl.carousel/assets/owl.carousel.min.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('build/libs/owl.carousel/assets/owl.theme.default.min.css')); ?>">
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('body'); ?>
+<body class="auth-body-bg">
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div>
+    <div class="container-fluid p-0">
+        <div class="row g-0">
+            <div class="col-xl-9">
+                <div class="auth-full-bg pt-lg-5 p-4">
+                    <div class="w-100">
+                        <div class="d-flex h-100 flex-column">
+                            <div class="p-4 mt-auto">
+                                <div class="row justify-content-center">
+                                    <div class="col-lg-7">
+                                        <div class="text-center">
+                                            <!-- Bisa ditambah carousel/testimonial kalau mau -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+
+            <div class="col-xl-3">
+                <div class="auth-full-page-content p-md-5 p-4">
+                    <div class="w-100">
+                        <div class="d-flex flex-column h-100">
+                            <div class="mb-4 mb-md-5">
+                                <a href="" class="d-block auth-logo">
+                                    <img src="<?php echo e(asset('images/logo1.png')); ?>" alt="Logo" height="90" class="auth-logo-dark">
+                                    <img src="<?php echo e(asset('images/logo1.png')); ?>" alt="Logo" height="70" class="auth-logo-light">
+                                </a>
+                            </div>
+                            <div class="my-auto">
+                                <div>
+                                    <h2 class="text-success">Selamat Datang</h2>
+                                    <p class="text-muted">Di Aplikasi Sistem Informasi Digital LP. Ma'arif NU PWNU DIY</p>
+                                </div>
+
+                                <div class="mt-4">
+                                    <form class="form-horizontal" method="POST" action="<?php echo e(route('login')); ?>">
+                                        <?php echo csrf_field(); ?>
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">Email <span class="text-danger">*</span></label>
+                                            <input name="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                                   value="<?php echo e(old('email')); ?>" id="username"
+                                                   placeholder="Enter Email" autocomplete="email" autofocus>
+                                            <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong><?php echo e($message); ?></strong>
+                                            </span>
+                                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label class="form-label">Password <span class="text-danger">*</span></label>
+                                            <div class="input-group auth-pass-inputgroup <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>">
+                                                <input type="password" name="password"
+                                                       class="form-control <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                                       id="userpassword" placeholder="Enter password"
+                                                       aria-label="Password" aria-describedby="password-addon">
+                                                <button class="btn btn-light" type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                                <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong><?php echo e($message); ?></strong>
+                                                </span>
+                                                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
+                                            <label class="form-check-label" for="remember">
+                                                Remember me
+                                            </label>
+                                        </div>
+
+                                        <div class="mt-3 d-grid">
+                                            <button class="btn btn-success waves-effect waves-light" type="submit">Log In</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div class="mt-4 mt-md-5 text-center">
+                                <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Nuist. Crafted by LP. Ma'arif NU PWNU DIY</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container-fluid -->
+</div>
+
+<style>
+    .auth-full-bg {
+        background: url("<?php echo e(asset('images/a.png')); ?>") no-repeat center center !important;
+        background-size: cover !important;
+    }
+    .bg-overlay {
+        background: none !important;
+    }
+</style>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('script'); ?>
+
+<script src="<?php echo e(asset('build/libs/jquery/jquery.min.js')); ?>"></script>
+<script src="<?php echo e(asset('build/libs/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(asset('build/libs/metismenu/metisMenu.min.js')); ?>"></script>
+<script src="<?php echo e(asset('build/libs/simplebar/simplebar.min.js')); ?>"></script>
+<script src="<?php echo e(asset('build/libs/node-waves/waves.min.js')); ?>"></script>
+<script src="<?php echo e(asset('build/libs/owl.carousel/owl.carousel.min.js')); ?>"></script>
+
+
+<script src="<?php echo e(asset('js/auth-2-carousel.init.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master-without-nav', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\apk_nuist\resources\views/auth/login.blade.php ENDPATH**/ ?>
