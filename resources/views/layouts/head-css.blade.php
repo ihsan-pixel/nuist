fef47362081fd067af79074682ad439275860eaf
muhamad ihsan <muhamadihsan06381@gmail.com>
2025-09-26 21:07:45 +0700

    bar

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -4,6 +4,8 @@
 <link href="{{ asset('build/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
 <link href="{{ asset('build/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- Material Design Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css">
 <!-- App Css-->
 <link href="{{ asset('build/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 <!-- App js -->
