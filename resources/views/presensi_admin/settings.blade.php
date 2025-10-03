a601268c42f28d5667f7f7143cc98196cdd7cfe1
muhamad ihsan <muhamadihsan06381@gmail.com>
2025-10-03 19:38:46 +0700

    .

--- resources/views/presensi_admin/settings.blade.php
+++ resources/views/presensi_admin/settings.blade.php
@@ -14,8 +14,8 @@
     @slot('title') Pengaturan Presensi @endslot
 @endcomponent
 
-<div class="row justify-content-center">
-    <div class="col-lg-10 col-xl-8">
+<div class="row">
+    <div class="col-12">
         <div class="card shadow border-0">
             <div class="card-header">
                 <h4 class="card-title mb-0">
@@ -37,9 +37,9 @@
                 </div>
                 @endif
 
-                    <div class="row">
+                    <div class="row gx-0">
                     @foreach($statuses as $status)
-                        <div class="col-md-6 mb-3">
+                        <div class="col-md-6 mb-3 px-1">
                             <div class="card border-primary shadow-sm h-100">
                                 <div class="card-header bg-primary text-white py-2">
                                     <h6 class="card-title mb-0">
@@ -124,7 +124,7 @@
                             </div>
                         </div>
                         @if ($loop->iteration % 2 == 0 && !$loop->last)
-                            </div><div class="row">
+                            </div><div class="row gx-0">
                         @endif
                     @endforeach
                     </div>
