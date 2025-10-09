dc43afcc21ce24772c93c9a09a1d04cf1a53ceb5
muhamad ihsan <muhamadihsan06381@gmail.com>
2025-10-09 16:20:01 +0700

    .

--- resources/views/teaching-schedules/index.blade.php
+++ resources/views/teaching-schedules/index.blade.php
@@ -6,7 +6,7 @@
 <div class="row">
     <div class="col-12">
         <div class="card">
-            <div class="card-header">
+            <div class="card-header d-flex justify-content-between align-items-center">
                 <h4 class="card-title">Daftar Jadwal Mengajar</h4>
                 @if(Auth::user()->role !== 'tenaga_pendidik')
                 <a href="{{ route('teaching-schedules.create') }}" class="btn btn-primary">Tambah Jadwal</a>
