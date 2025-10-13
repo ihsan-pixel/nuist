9c20be077b3b5eb3d73676f37ed364f93f923b84
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-13 13:36:47 +0700

    .

--- resources/views/presensi_admin/index.blade.php
+++ resources/views/presensi_admin/index.blade.php
@@ -409,7 +409,7 @@ $(document).ready(function () {
                 '</tr>';
         });
 
-        let avatarUrl = data.user.avatar ? '{{ asset('storage/avatars/') }}/' + data.user.avatar : '{{ asset('build/images/users/avatar-1.jpg') }}';
+        let avatarUrl = data.user.avatar ? '{{ asset('storage/') }}/' + data.user.avatar : '{{ asset('build/images/users/avatar-11.jpg') }}';
 
         let content = '<div class="container-fluid">' +
             '<div class="row g-2">' +
