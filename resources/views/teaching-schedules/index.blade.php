57b8b16c4ea070a80cf560ffdf67e3452442ccee
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-15 10:00:23 +0700

    .

--- resources/views/teaching-schedules/index.blade.php
+++ resources/views/teaching-schedules/index.blade.php
@@ -80,11 +80,7 @@
                             @if(Auth::user()->role !== 'tenaga_pendidik')
                             <td>
                                 <a href="{{ route('teaching-schedules.edit', $schedule->id) }}" class="btn btn-sm btn-warning">Edit</a>
-                                <form action="{{ route('teaching-schedules.destroy', $schedule->id) }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Yakin hapus?')">Hapus</button>
-                                </form>
+                                <button type="button" class="btn btn-sm btn-danger delete-btn" data-id="{{ $schedule->id }}" data-name="{{ $schedule->subject }} - {{ $schedule->class_name }}">Hapus</button>
                             </td>
                             @endif
                         </tr>
@@ -297,6 +293,35 @@ $(document).ready(function() {
             confirmButtonText: 'OK'
         });
     @endif
+
+    // SweetAlert for delete confirmation
+    $('.delete-btn').on('click', function() {
+        var scheduleId = $(this).data('id');
+        var scheduleName = $(this).data('name');
+
+        Swal.fire({
+            title: 'Yakin hapus?',
+            text: 'Jadwal "' + scheduleName + '" akan dihapus secara permanen!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, Hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Create and submit form
+                var form = $('<form>', {
+                    'method': 'POST',
+                    'action': '{{ route("teaching-schedules.destroy", ":id") }}'.replace(':id', scheduleId)
+                });
+                form.append('<input type="hidden" name="_token" value="{{ csrf_token() }}">');
+                form.append('<input type="hidden" name="_method" value="DELETE">');
+                $('body').append(form);
+                form.submit();
+            }
+        });
+    });
 });
 </script>
 @endsection
