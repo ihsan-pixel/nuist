99443bee4d57afbf44e9c7876d2521bf3db766ce
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-17 10:22:13 +0700

    .

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © NUIST. All Rights Reserved. Version 1.0.0
-
+                <script>document.write(new Date().getFullYear())</script> © NUIST. All Rights Reserved. Version 1.0.2
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
