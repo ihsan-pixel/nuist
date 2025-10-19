4c0918954278be4cf8e4563b5b1521fe1fb4735c
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-19 10:47:40 +0700

    .

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,7 +8,6 @@
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title" key="t-menu">@lang('translation.Menu')</li>
-                <li class="menu-title" key="t-menu">@lang('translation.Menu')</li>
 
                 <li>
                     <a href="{{ url('dashboard') }}" class="waves-effect">
