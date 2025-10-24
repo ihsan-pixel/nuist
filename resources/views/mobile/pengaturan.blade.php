f43a24656eb5d875cb3088258aaf30139479591a
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-24 16:29:20 +0700

    .

--- resources/views/mobile/pengaturan.blade.php
+++ resources/views/mobile/pengaturan.blade.php
@@ -194,7 +194,7 @@
                 <h6 class="mb-1">Pengaturan</h6>
                 <h5 class="fw-bold mb-0">{{ $user->name }}</h5>
             </div>
-          <img src="{{ isset($user->avatar) ? asset('storage/' . $user->avatar) : asset('build/images/users/avatar-11.jpg') }}"
+          <img src="{{ isset($user->avatar) ? asset('storage/app/public/' . $user->avatar) : asset('build/images/users/avatar-11.jpg') }}"
                  class="rounded-circle border border-white" width="32" height="32" alt="User">
         </div>
     </div>
