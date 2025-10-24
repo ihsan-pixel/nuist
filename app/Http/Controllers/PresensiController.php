fef34654d6c7cee80d47fa9b48435168347824cf
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-24 09:24:11 +0700

    .

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -244,7 +244,7 @@ class PresensiController extends Controller
             $batasPulang = $timeRanges['pulang_start'];
             // Adjust for special users
             if ($user->role === 'tenaga_pendidik' && !$user->pemenuhan_beban_kerja_lain) {
-                $batasAkhirMasuk = '12:00';
+                $batasAkhirMasuk = '08:00';
             }
         }
 
