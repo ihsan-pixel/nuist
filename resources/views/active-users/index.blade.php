989ae1aa5f754c9c42813be26bba58f28a0a26e8
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-10-25 11:59:34 +0700

    .

--- resources/views/active-users/index.blade.php
+++ resources/views/active-users/index.blade.php
@@ -142,7 +142,7 @@
                     @forelse($users as $user)
                     <div class="user-item">
                         <div class="d-flex align-items-center">
-                            <img src="{{ $user->avatar ? asset('storage/app/public/' . $user->avatar) : asset('build/images/users/images/avatar-11.jpg') }}"
+                            <img src="{{ $user->avatar ? asset('storage/app/public/' . $user->avatar) : asset('build/images/users/avatar-11.jpg') }}"
                                  alt="Avatar" class="user-avatar me-3">
                             <div class="flex-grow-1">
                                 <h6 class="mb-1">{{ $user->name }}</h6>
