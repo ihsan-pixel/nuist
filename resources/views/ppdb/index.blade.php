deccbebc78d632bf1262b5cec869b18edde7fcc4
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-14 09:10:05 +0700

    .

--- resources/views/ppdb/index.blade.php
+++ resources/views/ppdb/index.blade.php
@@ -11,7 +11,7 @@
         background-position: center;
         background-attachment: fixed;
         color: #0f854a;
-        min-height: 80vh;
+        min-height: 90vh;
         display: flex;
         align-items: center;
     }
