7659eef0aa6c4f1f1958377d1cb41f91833f38f7
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-14 12:40:20 +0700

    .

--- resources/views/ppdb/index.blade.php
+++ resources/views/ppdb/index.blade.php
@@ -13,7 +13,7 @@
 
         /* FIX TERPENTING */
         display: block; /* buang flex agar elemen tidak ketarik tengah */
-        padding-top: 80px; /* beri ruang flayer lebih kecil */
+        padding-top: 40px; /* beri ruang flayer lebih kecil */
         padding-bottom: 80px;
         min-height: 93vh;
         position: relative;
