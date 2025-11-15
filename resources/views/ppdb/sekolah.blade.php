8ffb5eadb76a2ddfba14113067444db978f2806a
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-15 15:35:51 +0700

    .

--- resources/views/ppdb/sekolah.blade.php
+++ resources/views/ppdb/sekolah.blade.php
@@ -799,14 +799,15 @@
                     <p class="lead mb-4">{{ $madrasah->deskripsi_singkat }}</p>
                 @endif
                 <div class="d-flex gap-3 flex-wrap">
-                    @if(isset($ppdb->slug) && $ppdb->slug)
-                        <a href="{{ route('ppdb.daftar', $ppdb->slug) }}" class="btn btn-ppdb">
+                    @php $currentSlug = request()->route('slug'); @endphp
+                    @if($currentSlug)
+                        <a href="{{ route('ppdb.daftar', $currentSlug) }}" class="btn btn-ppdb">
                             <i class="fas fa-edit me-2"></i>Daftar Sekarang
                         </a>
                         <a href="#jurusan" class="btn btn-orange btn-lg px-4">
                             <i class="fas fa-graduation-cap me-2"></i>Lihat Jurusan
                         </a>
-                        <a href="{{ route('ppdb.daftar', $ppdb->slug) }}" class="btn btn-outline-light btn-lg px-4">
+                        <a href="{{ route('ppdb.daftar', $currentSlug) }}" class="btn btn-outline-light btn-lg px-4">
                             <i class="fas fa-user-plus me-2"></i>Daftar
                         </a>
                     @else
