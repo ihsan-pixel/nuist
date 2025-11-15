631f0112136f8724d0d3642ce5f2154951febaef
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-15 22:58:38 +0700

    .

--- resources/views/ppdb/dashboard/lp.blade.php
+++ resources/views/ppdb/dashboard/lp.blade.php
@@ -59,7 +59,7 @@
 
     .stat-card.total-sekolah {
         background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
-        color: white;
+        color: rgb(242, 0, 0);
     }
 
     .stat-card.total-pendaftar {
