2086c83da3e8a8417100c06e323dc6a30de28b43
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-16 10:22:05 +0700

    .

--- resources/views/ppdb/dashboard/lp.blade.php
+++ resources/views/ppdb/dashboard/lp.blade.php
@@ -739,7 +739,7 @@
                                                 <a href="{{ route('ppdb.lp.ppdb-settings', $detail['sekolah']->id) }}" class="btn btn-outline-info btn-sm">
                                                     <i class="mdi mdi-cog me-1"></i>Pengaturan PPDB
                                                 </a>
-                                                <a href="{{ route('ppdb.lp.edit', $detail['sekolah']->id) }}" class="btn btn-outline-primary btn-sm text-white">
+                                                <a href="{{ route('ppdb.lp.edit', $detail['sekolah']->id) }}" class="btn btn-outline-primary btn-sm">
                                                     <i class="mdi mdi-pencil me-1"></i>Edit Profile
                                                 </a>
                                             </div>
