9b9c60231e76d988fc2091a6f6ced7f4b44853d9
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-21 15:08:03 +0700

    .

--- resources/views/pdf/presensi-rekap.blade.php
+++ resources/views/pdf/presensi-rekap.blade.php
@@ -40,7 +40,7 @@
             <td>{{ $p->waktu_keluar }}</td>
             <td>
                 @if ($p->selfie_keluar_path && file_exists(public_path('storage/' . $p->selfie_keluar_path)))
-                    <img src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path('storage/' . $p->selfie_keluar_path))) }}">
+                    <img src="file://{{ public_path('storage/' . $p->selfie_keluar_path) }}">
                 @endif
             </td>
 
