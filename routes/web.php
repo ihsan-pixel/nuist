d952505456950751c3e26156d88dfc67d8fadabe
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-21 12:36:11 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/presensi-admin/madrasah/{madrasahId}', [PresensiAdminController::class, 'showMadrasahDetail'])->name('presensi_admin.show_detail');
     Route::get('/presensi-admin/export', [PresensiAdminController::class, 'export'])->name('presensi_admin.export');
     Route::get('/presensi-admin/export-madrasah/{madrasahId}', [PresensiAdminController::class, 'exportMadrasah'])->name('presensi_admin.export_madrasah');
+    Route::get('/presensi-admin/export-excel', [PresensiAdminController::class, 'exportExcel'])->name('presensi_admin.export_excel');
 
     // Teaching Progress Routes - Super Admin Only
     Route::middleware(['role:super_admin'])->group(function () {
