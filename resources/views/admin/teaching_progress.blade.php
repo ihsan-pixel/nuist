6537771425b40c9d491a74fda3d3509523424c2e
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-25 10:01:01 +0700

    .

--- resources/views/admin/teaching_progress.blade.php
+++ resources/views/admin/teaching_progress.blade.php
@@ -343,7 +343,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Detail Presensi Mengajar</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
             </div>
             <div class="modal-body">
                 <div class="row mb-3">
@@ -378,7 +378,7 @@
                 </table>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                {{-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button> --}}
             </div>
         </div>
     </div>
