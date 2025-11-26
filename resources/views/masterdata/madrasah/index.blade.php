de6094007bb931e28b1aa1c3ada9f94920f2013c
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-11-26 16:14:56 +0700

    .

--- resources/views/masterdata/madrasah/index.blade.php
+++ resources/views/masterdata/madrasah/index.blade.php
@@ -624,7 +624,8 @@
     <script src="{{ asset('build/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
     {{-- Leaflet scripts for polygon editing --}}
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js"></script>
+    {{-- <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js" integrity="sha512-ozq65x0Vw2rFeEbSpgDJUb+xBzvfqn6U9G3fjEWr0y+qdcWVgJRapgKoEHLmZKLEd5X6lxCOJLQKFkRDuM+C8A==" crossorigin=""></script>
 
     <script>
