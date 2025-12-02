59c451395f05cc9d5d1cf0bbabf6684c4209fefd
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-03 04:12:09 +0700

    .

--- resources/views/ppdb/daftar.blade.php
+++ resources/views/ppdb/daftar.blade.php
@@ -693,6 +693,40 @@
                                 id="provinsi" name="provinsi" required>
                             <option value="">-- Pilih Provinsi --</option>
                             <!-- Provinsi akan diisi melalui JavaScript -->
+                            <option value="11">ACEH</option>
+                            <option value="12">SUMATERA UTARA</option>
+                            <option value="13">SUMATERA BARAT</option>
+                            <option value="14">RIAU</option>
+                            <option value="15">JAMBI</option>
+                            <option value="16">SUMATERA SELATAN</option>
+                            <option value="17">BENGKULU</option>
+                            <option value="18">LAMPUNG</option>
+                            <option value="19">KEPULAUAN BANGKA BELITUNG</option>
+                            <option value="21">KEPULAUAN RIAU</option>
+                            <option value="31">DKI JAKARTA</option>
+                            <option value="32">JAWA BARAT</option>
+                            <option value="33">JAWA TENGAH</option>
+                            <option value="34">DI YOGYAKARTA</option>
+                            <option value="35">JAWA TIMUR</option>
+                            <option value="36">BANTEN</option>
+                            <option value="51">BALI</option>
+                            <option value="52">NUSA TENGGARA BARAT</option>
+                            <option value="53">NUSA TENGGARA TIMUR</option>
+                            <option value="61">KALIMANTAN BARAT</option>
+                            <option value="62">KALIMANTAN TENGAH</option>
+                            <option value="63">KALIMANTAN SELATAN</option>
+                            <option value="64">KALIMANTAN TIMUR</option>
+                            <option value="65">KALIMANTAN UTARA</option>
+                            <option value="71">SULAWESI UTARA</option>
+                            <option value="72">SULAWESI TENGAH</option>
+                            <option value="73">SULAWESI SELATAN</option>
+                            <option value="74">SULAWESI TENGGARA</option>
+                            <option value="75">GORONTALO</option>
+                            <option value="76">SULAWESI BARAT</option>
+                            <option value="81">MALUKU</option>
+                            <option value="82">MALUKU UTARA</option>
+                            <option value="91">PAPUA BARAT</option>
+                            <option value="92">PAPUA</option>
                         </select>
                         @error('provinsi')
                             <div class="invalid-feedback">{{ $message }}</div>
