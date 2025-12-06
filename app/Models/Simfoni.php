2a254da63a371d5656993a26ee8bc89582cfa075
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-06 09:58:45 +0700

    .

--- app/Models/Simfoni.php
+++ app/Models/Simfoni.php
@@ -53,6 +53,44 @@ class Simfoni extends Model
         'penghasilan_lain',
         'penghasilan_pasangan',
         'total_penghasilan',
+        'masa_kerja',
+        'kategori_penghasilan',
+
+        // E. STATUS KEKADERAN
+        'status_kader_diri',
+        'pendidikan_kader',
+        'status_kader_ayah',
+        'status_kader_ibu',
+        'status_kader_pasangan',
+
+        // F. DATA KELUARGA
+        'nama_ayah',
+        'nama_ibu',
+        'nama_pasangan',
+        'jumlah_anak',
+
+        // G. PROYEKSI KE DEPAN
+        'akan_kuliah_s2',
+        'akan_mendaftar_pns',
+        'akan_mendaftar_pppk',
+        'akan_mengikuti_ppg',
+        'akan_menulis_buku_modul_riset',
+        'akan_mengikuti_seleksi_diklat_cakep',
+        'akan_membimbing_riset_prestasi_siswa',
+        'akan_masuk_tim_unggulan_sekolah_madrasah',
+        'akan_kompetisi_pimpinan_level_ii',
+        'akan_aktif_mengikuti_pelatihan',
+        'akan_aktif_mgmp_mkk',
+        'akan_mengikuti_pendidikan_kader_nu',
+        'akan_aktif_membantu_kegiatan_lembaga',
+        'akan_aktif_mengikuti_kegiatan_nu',
+        'akan_aktif_ikut_zis_kegiatan_sosial',
+        'akan_mengembangkan_unit_usaha_satpen',
+        'akan_bekerja_disiplin_produktif',
+        'akan_loyal_nu_aktif_masyarakat',
+        'akan_bersedia_dipindah_satpen_lain',
+        'skor_proyeksi',
+        'pernyataan_setuju',
     ];
 
     protected $casts = [
