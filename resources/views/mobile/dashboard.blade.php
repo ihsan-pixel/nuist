67fa443f4a8ed84752bb4822912f2c52f03737e4
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-18 15:50:27 +0700

    .

--- resources/views/mobile/dashboard.blade.php
+++ resources/views/mobile/dashboard.blade.php
@@ -119,7 +119,7 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
-            font-size: 8px;
+            font-size: 10px;
             line-height: 1.4;
             margin-bottom: 2px;
         }
