62b833a384d21cdaa34695930bf032d923f2e10c
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-18 12:48:23 +0700

    .

--- resources/views/mobile/dashboard.blade.php
+++ resources/views/mobile/dashboard.blade.php
@@ -58,7 +58,7 @@
     <style>
         body {
             background: #f8f9fb url('{{ asset("images/bg.png") }}') no-repeat center center fixed;
-            background-size: auto;
+            background-size: cover;
             font-family: 'Poppins', sans-serif;
             font-size: 13px;
         }
