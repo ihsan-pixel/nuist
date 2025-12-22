2fd533a84c03181ea10455c08bb408e451d05b52
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-22 07:29:31 +0700

    .

--- resources/views/mobile/dashboard.blade.php
+++ resources/views/mobile/dashboard.blade.php
@@ -1075,7 +1075,7 @@ $progressColor = "rgb($red, $green, 0)";
         </div>
 
         <div class="text-center mt-2" style="font-size: 10px">
-            <a href="#" class="text-light text-decoration-none" data-bs-toggle="collapse" data-bs-target="#performanceAccordion" aria-expanded="false" aria-controls="performanceAccordion" style="font-size: 12px; font-weight: 500;">
+            <a href="#" class="text-light text-decoration-none" data-bs-toggle="collapse" data-bs-target="#performanceAccordion" aria-expanded="false" aria-controls="performanceAccordion" style="font-size: 10px; font-weight: 500;">
                 Lihat Detail <i class="bx bx-chevron-down" id="detailArrow"></i>
             </a>
         </div>
