b068fb01a37456802495311475b5dd77bd0018b4
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-22 08:00:04 +0700

    .

--- resources/views/mobile/dashboard.blade.php
+++ resources/views/mobile/dashboard.blade.php
@@ -1569,7 +1569,7 @@ $progressColor = "rgb($red, $green, 0)";
 
     <!-- Information Section -->
     <div class="schedule-section">
-        {{-- <h6 class="section-title">Informasi Aplikasi</h6> --}}
+        <h6 class="section-title">Informasi Aplikasi</h6>
         <!-- Content will be added here -->
     </div>
 
