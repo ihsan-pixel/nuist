85d89e9880822674c132c8f1818e2ce0a3571849
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2025-12-30 09:03:29 +0700

    .

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NU IST - Sistem Informasi Tenaga Pendidik Madrasah</title>
+    <title>NUIST - Sistem Informasi Tenaga Pendidik Madrasah</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
