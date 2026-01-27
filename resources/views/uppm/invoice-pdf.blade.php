083d0800172732e5a53aab0c06103f87563bbc3b
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-01-27 10:01:24 +0700

    .

--- resources/views/uppm/invoice-pdf.blade.php
+++ resources/views/uppm/invoice-pdf.blade.php
@@ -2,7 +2,7 @@
     <table style="width: 100%; border: none; margin-bottom: 1rem;">
         <tr>
             <td style="width: 30%; padding: 0; vertical-align: top;">
-                <img src="{{ asset('images/logo1.png') }}" alt="" height="50">
+                <img src="{{ public_path('images/logo1.png') }}" height="50">
             </td>
             <td style="width: 70%; padding: 0; vertical-align: top; text-align: right;">
                 <h1 style="margin-top: 1rem; font-size: 24px;">INVOICE PEMBAYARAN UPPM</h1>
