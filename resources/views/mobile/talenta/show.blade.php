232b17b0d394d797666cf09d24bfb4b62174a5d0
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-02-05 13:08:34 +0700

    .

--- resources/views/mobile/talenta/show.blade.php
+++ resources/views/mobile/talenta/show.blade.php
@@ -287,7 +287,7 @@
             @if($talenta->pkpnu_sertifikat)
             <div class="data-item">
                 <label>Sertifikat PKPNU/PDPKPNU:</label>
-                <a href="/storage/app/public/talenta{{ $talenta->pkpnu_sertifikat }}" target="_blank" class="btn btn-sm btn-outline-primary">Lihat File</a>
+                <a href="/storage/app/public/talenta/{{ $talenta->pkpnu_sertifikat }}" target="_blank" class="btn btn-sm btn-outline-primary">Lihat File</a>
             </div>
             @endif
             @if($talenta->mknu_sertifikat)
