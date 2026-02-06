5d685971ceed9dfac5cf9190ae2fca3996c5bb22
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-02-06 11:21:03 +0700

    .

--- resources/views/pdf/laporan-akhir-tahun-template.blade.php
+++ resources/views/pdf/laporan-akhir-tahun-template.blade.php
@@ -574,7 +574,7 @@
                 <img src="{{ $laporan->signature_data }}" alt="Tanda Tangan" style="max-width: 200px; height: auto;">
             </div>
         @endif
-        <p><strong>{{ $laporan->nama_kepala_sekolah_madrasah ?? 'N/A' }}{{$laporan->gelar}}</strong></p>
+        <p><strong>{{ $laporan->nama_kepala_sekolah_madrasah ?? 'N/A' }}, {{$laporan->gelar}}</strong></p>
         <div class="signature-line" style="margin-top: -10px;"></div>
         <p style="margin-top: -20px">NIP. {{ $laporan->user->nip ?? '-' }}</p>
     </div>
