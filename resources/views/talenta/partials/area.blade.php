8985b7d2feefdd1d04e03cb561f825432c7b0e77
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-02-11 12:19:33 +0700

    .

--- resources/views/talenta/partials/area.blade.php
+++ resources/views/talenta/partials/area.blade.php
@@ -9,32 +9,33 @@
             'tanggal' => $materi->tanggal_materi
         ])
 
+    @else
+
+        {{-- SUB TAB --}}
+        <div class="sub-tabs">
+
+            <button class="sub-tab-btn active"
+                    onclick="openSubTab(event,'{{ $config['slug'] }}-onsite')">
+                On Site
+            </button>
+
+            <button class="sub-tab-btn"
+                    onclick="openSubTab(event,'{{ $config['slug'] }}-terstruktur')">
+                Terstruktur
+            </button>
+
+            <button class="sub-tab-btn"
+                    onclick="openSubTab(event,'{{ $config['slug'] }}-kelompok')">
+                Kelompok
+            </button>
+
+        </div>
+
+
+        @include('talenta.partials.forms.onsite', ['config' => $config])
+        @include('talenta.partials.forms.terstruktur', ['config' => $config])
+        @include('talenta.partials.forms.kelompok', ['config' => $config])
+
     @endif
-
-
-    {{-- SUB TAB --}}
-    <div class="sub-tabs">
-
-        <button class="sub-tab-btn active"
-                onclick="openSubTab(event,'{{ $config['slug'] }}-onsite')">
-            On Site
-        </button>
-
-        <button class="sub-tab-btn"
-                onclick="openSubTab(event,'{{ $config['slug'] }}-terstruktur')">
-            Terstruktur
-        </button>
-
-        <button class="sub-tab-btn"
-                onclick="openSubTab(event,'{{ $config['slug'] }}-kelompok')">
-            Kelompok
-        </button>
-
-    </div>
-
-
-    @include('talenta.partials.forms.onsite', ['config' => $config])
-    @include('talenta.partials.forms.terstruktur', ['config' => $config])
-    @include('talenta.partials.forms.kelompok', ['config' => $config])
 
 </div>
