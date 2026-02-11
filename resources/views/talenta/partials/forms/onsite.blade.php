f5a5e764dde088e5a20091d83d268c8fd95999bd
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-02-11 12:11:31 +0700

    .

--- resources/views/talenta/partials/forms/onsite.blade.php
+++ resources/views/talenta/partials/forms/onsite.blade.php
@@ -13,33 +13,10 @@
 <input type="hidden" name="area" value="{{ $config['slug'] }}">
 <input type="hidden" name="jenis_tugas" value="on_site">
 
-@if($config['slug'] === 'kepemimpinan')
-    <div class="mb-3">
-        <label for="konteks" class="form-label">Konteks</label>
-        <textarea name="konteks" class="form-control" required></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="peran" class="form-label">Peran</label>
-        <input type="text" name="peran" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label for="nilai_kepemimpinan" class="form-label">Nilai Kepemimpinan</label>
-        <textarea name="nilai_kepemimpinan" class="form-control" required></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="masalah_kepemimpinan" class="form-label">Masalah Kepemimpinan</label>
-        <textarea name="masalah_kepemimpinan" class="form-control" required></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="pelajaran_penting" class="form-label">Pelajaran Penting</label>
-        <textarea name="pelajaran_penting" class="form-control" required></textarea>
-    </div>
-@else
     <input type="file"
            name="lampiran"
            class="form-control"
            required>
-@endif
 
 <div class="d-flex justify-content-end mt-3">
     @if($config['slug'] === 'kepemimpinan' || !isset($existingTasks[$config['slug']]))
