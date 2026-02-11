d150130b3343af5afb90406c453f9b37783c7e82
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-02-11 20:22:49 +0700

    .

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -342,6 +342,12 @@
                     </a>
                 </li>
                 <li>
+                    <a href="{{ route('instumen-talenta.index') }}" class="waves-effect">
+                        <i class="bx bx-data"></i>
+                        <span>Talenta</span>
+                    </a>
+                </li>
+                <li>
                     <a href="{{ route('admin.laporan-akhir-tahun.index') }}" class="waves-effect">
                         <i class="bx bx-data"></i>
                         <span>Laporan Akhir Tahun</span>
