d28ca84fca3299eb5b10e15ed763d86d997f05b5
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-03-02 15:14:47 +0700

    .

--- mobile-capacitor/capacitor.config.ts
+++ mobile-capacitor/capacitor.config.ts
@@ -3,7 +3,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nuist.mobile',
   appName: 'Nuist Mobile',
-  webDir: '../public', // load Laravel public folder from the project root
+  webDir: '../public',
+
+  server: {
+    allowNavigation: ['nuist.id'] // 🔥 WAJIB supaya tetap di dalam app
+  }
 };
 
 export default config;
