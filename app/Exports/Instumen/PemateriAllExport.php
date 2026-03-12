88dd12fc0ec8cc70b94e1420dfffafdf31b84772
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-03-12 11:05:45 +0700

    .

--- app/Exports/Instumen/PemateriAllExport.php
+++ app/Exports/Instumen/PemateriAllExport.php
@@ -2,34 +2,75 @@
 
 namespace App\Exports\Instumen;
 
-use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use App\Models\TalentaPemateri;
+use App\Models\TalentaPenilaianTrainer;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class PemateriAllExport implements WithMultipleSheets
+class PemateriAllExport implements FromCollection, WithHeadings
 {
-    public function sheets(): array
+    public function headings(): array
     {
-        $sheets = [];
-        $pemateris = TalentaPemateri::all();
-        foreach ($pemateris as $p) {
-            $sheets[] = new PemateriSheetWithTitle($p->id, $p->nama ?? 'Pemateri_'.$p->id);
+        return [
+            'Kode Pemateri',
+            'Pemateri',
+            'Evaluator',
+            'Kualitas Materi',
+            'Penyampaian',
+            'Integrasi Kasus',
+            'Penjelasan',
+            'Umpan Balik',
+            'Waktu',
+            'Entri Count',
+        ];
+    }
+
+    public function collection(): Collection
+    {
+        $pemateris = TalentaPemateri::orderBy('nama')->get();
+        $rows = collect();
+
+        foreach ($pemateris as $pemateri) {
+            $entries = TalentaPenilaianTrainer::with('user')
+                ->where('talenta_pemateri_id', $pemateri->id)
+                ->get()
+                ->groupBy('user_id');
+
+            if ($entries->isEmpty()) {
+                $rows->push([
+                    $pemateri->kode_pemateri ?? '-',
+                    $pemateri->nama ?? 'Pemateri ID:' . $pemateri->id,
+                    '-',
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    0,
+                ]);
+
+                continue;
+            }
+
+            foreach ($entries as $userId => $group) {
+                $user = $group->first()->user;
+                $rows->push([
+                    $pemateri->kode_pemateri ?? '-',
+                    $pemateri->nama ?? 'Pemateri ID:' . $pemateri->id,
+                    $user ? $user->name : 'User ID:' . $userId,
+                    round($group->avg('kualitas_materi'), 2),
+                    round($group->avg('penyampaian'), 2),
+                    round($group->avg('integrasi_kasus'), 2),
+                    round($group->avg('penjelasan'), 2),
+                    round($group->avg('umpan_balik'), 2),
+                    round($group->avg('waktu'), 2),
+                    $group->count(),
+                ]);
+            }
         }
-        return $sheets;
-    }
-}
 
-class PemateriSheetWithTitle extends PemateriSheetExport implements \Maatwebsite\Excel\Concerns\WithTitle
-{
-    protected $title;
-
-    public function __construct($pemateriId, $title)
-    {
-        parent::__construct($pemateriId);
-        $this->title = substr($title, 0, 31);
-    }
-
-    public function title(): string
-    {
-        return $this->title;
+        return $rows;
     }
 }
