14b0b71b56bbc75957f2f79d607dbe2cd93d97c8
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-04-10 10:07:06 +0700

    .

--- resources/views/mobile/laporan-persentase-kehadiran.blade.php
+++ resources/views/mobile/laporan-persentase-kehadiran.blade.php
@@ -5,9 +5,9 @@
 
 @section('content')
 <div class="d-flex align-items-center mb-3" style="margin-top: -10px;">
-    <button onclick="history.back()" class="btn btn-link text-decoration-none p-0 me-2" style="color: #004b4c;">
+    <a href="{{ route('mobile.laporan') }}" class="btn btn-link text-decoration-none p-0 me-2" style="color: #004b4c;">
         <i class="bx bx-arrow-back" style="font-size: 20px;"></i>
-    </button>
+    </a>
     <span class="fw-bold" style="color: #004b4c; font-size: 12px;">Kembali</span>
 </div>
 
