c3c193afa7ffcb6203e0160e0eb227f2ef0d35de
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-04-30 15:33:36 +0700

    .

--- nuist_flutter/lib/main.dart
+++ nuist_flutter/lib/main.dart
@@ -1,46 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'src/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Login NUIST")),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(labelText: "Email"),
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Login"),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(
+    const ProviderScope(
+      child: NuistApp(),
+    ),
+  );
 }
