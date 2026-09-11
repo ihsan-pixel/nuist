06a166304d7e221f092cfdc51a9cd562aa9a721c
LPMNUDIY macpro <lpmnudiymacpro@LPMNUDIYs-MacBook-Pro.local>
2026-09-11 13:19:45 +0700

    .

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn" onclick="document.body.classList.toggle('sidebar-enable'); if (window.innerWidth >= 992) { document.body.classList.toggle('vertical-collpsed'); } else { document.body.classList.remove('vertical-collpsed'); }">
+            <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn" aria-label="Buka atau kecilkan sidebar">
                 <i class="fa fa-fw fa-bars"></i>
             </button>
 
